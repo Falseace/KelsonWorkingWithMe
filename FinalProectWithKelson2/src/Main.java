e1cc096ad2a45f814a25dd4f0c278456da5e5030
Falseace <falseace@gmail.com>
2018-05-10 14:26:15 -0600

    I did WORK

---
+++ FinalProectWithKelson2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Is github actually working the way I think it is");
+    }
+}
--- FinalProjectWithKelson/src/com/company/MazeGame.java
+++ FinalProjectWithKelson/src/com/company/MazeGame.java
@@ -82,8 +82,8 @@ public class MazeGame {
     };
 
     public static void main(String[] args) {
-        int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 8;
-        int y = args.length == 2 ? (Integer.parseInt(args[1])) : 8;
+        int x = 10;
+        int y = 10;
         MazeGame maze = new MazeGame(x, y);
         maze.display();
     }
